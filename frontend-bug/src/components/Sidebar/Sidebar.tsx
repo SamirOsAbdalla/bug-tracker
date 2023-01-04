a4e9a8886e3ba52216108af6a7128ed938f2e760
samir01 <samir.os.abdalla@gmail.com>
2023-01-04 14:58:36 -0800

    Created simple backend to run node server with typescript

--- frontend-bug/src/components/Sidebar/Sidebar.tsx
+++ frontend-bug/src/components/Sidebar/Sidebar.tsx
@@ -5,13 +5,36 @@ import { MdOutlineDashboard } from "react-icons/md"
 import { AiOutlineProject } from "react-icons/ai"
 import { IoTicketOutline } from "react-icons/io5"
 export const Sidebar = () => {
+
+    const handleSidebarChange = () => {
+        const hamburger = document.querySelector(".sidebar__hamburger") as HTMLElement
+        const wrapper = document.querySelector(".sidebar__wrapper") as HTMLElement
+        const welcome = document.querySelector(".sidebar__welcome") as HTMLElement
+        const icon = document.querySelector(".sidebar__icon") as HTMLElement
+        const text = document.querySelectorAll<HTMLElement>(".sidebar__text") as NodeList
+        const login = document.querySelector(".sidebar__login") as HTMLElement
+        hamburger.classList.toggle("active")
+        wrapper.classList.toggle("wrapper__toggle")
+        welcome.classList.toggle("welcome__toggle")
+        icon.classList.toggle("icon__toggle")
+        text.forEach((node) => {
+            const transformedNode = node as HTMLElement
+            transformedNode.classList.toggle("text__toggle")
+        })
+        login.classList.toggle("login__toggle")
+    }
     return (
-        <nav className="sidebar__wrapper">
+        <nav className="sidebar__wrapper wrapper__toggle special__toggle">
+            <div onClick={handleSidebarChange} className="sidebar__hamburger">
+                <span className="sidebar__bar"></span>
+                <span className="sidebar__bar"></span>
+                <span className="sidebar__bar"></span>
+            </div>
             <header className="sidebar__header">
                 <div className="ladybug__container">
                     <img className="ladybug__image" src={require("../../images/bee.png")} />
                 </div>
-                <div className="sidebar__welcome">
+                <div className="sidebar__welcome welcome__toggle toggle">
                     <span>Welcome,</span>
                     <span>User!</span>
                 </div>
@@ -19,20 +42,20 @@ export const Sidebar = () => {
             <div className="sidebar__links">
                 <ul className="sidebar__list">
                     <NavLink className="sidebar__link" to="/dashboard">
-                        <MdOutlineDashboard />
-                        <span>Dashboard</span>
+                        <MdOutlineDashboard className="sidebar__icon icon__toggle toggle" />
+                        <span className="sidebar__text text__toggle toggle">Dashboard</span>
                     </NavLink>
                     <NavLink className="sidebar__link" to="/dashboard">
-                        <AiOutlineProject />
-                        <span>Projects</span>
+                        <AiOutlineProject className="sidebar__icon icon__toggle toggle" />
+                        <span className="sidebar__text text__toggle toggle">Projects</span>
                     </NavLink>
                     <NavLink className="sidebar__link" to="/dashboard">
-                        <IoTicketOutline />
-                        <span>Tickets</span>
+                        <IoTicketOutline className="sidebar__icon icon__toggle toggle" />
+                        <span className="sidebar__text text__toggle toggle">Tickets</span>
                     </NavLink>
                 </ul>
             </div>
-            <button className="sidebar__login">
+            <button className="sidebar__login login__toggle toggle">
                 Login
             </button>
         </nav>
